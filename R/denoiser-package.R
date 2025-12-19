c32f9bf4557b037f70923a45143dbd140b28025e
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2025-12-19 13:37:37 +0100

    Add basic package information

---
+++ R/denoiser-package.R
@@ -0,0 +1,13 @@
+#' @description
+#' Noiser and denoiser functions to be used for preprocessing spatial data. 
+#' Based on the calibration project of the M4MA team.
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @useDynLib denoiser
+#' @importFrom Rcpp evalCpp
+#' @exportPattern "^[[:alpha:]]+"
+## usethis namespace: end
+NULL
