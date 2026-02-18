75c8dfb0b215d2f498a9ea45fe41969169ca7ffa
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2026-02-18 17:25:17 +0100

    Make Kalman filter compatible with models

--- R/kalman_filter.R
+++ R/kalman_filter.R
@@ -179,8 +179,8 @@ kalman_filter <- function(data,
                     # These parameters are F (the deterministic movement) and 
                     # W (the expected stochastic noise around this movement), 
                     # both of which depend on the time between observations.
-                    F <- parameters[["F"]](z$delta_t[i])
-                    W <- parameters[["W"]](z$delta_t[i])
+                    F <- parameters[["F"]](i)
+                    W <- parameters[["W"]](i)
 
                     # Perform the prediction itself.
                     prediction <- kf_predict(
