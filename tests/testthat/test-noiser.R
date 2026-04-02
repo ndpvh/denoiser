8d2c55b1b54406297af365cde144870e1869b54d
Niels Vanhasbroeck <niels.vanhasbroeck@gmail.com>
2026-04-02 17:19:41 +0200

    Add test for retaining columns in `noiser` (Issue #1)

--- tests/testthat/test-noiser.R
+++ tests/testthat/test-noiser.R
@@ -18,3 +18,38 @@ test_that(
         expect_equal(tst, data)
     }
 )
+
+test_that(
+    "Retaining additional columns in noiser works",
+    {
+        # Create a data.frame
+        data <- data.frame(
+            time = 1:100,
+            x = 1:100,
+            y = 1:100,
+            column_1 = 2 * (1:100)
+        )
+
+        # Provide your own function to noiser
+        tst <- noiser(
+            data,
+            cols = c(
+                "time" = "time",
+                "x" = "x",
+                "y" = "y",
+                "var_1" = "column_1"
+            ),
+            model = "independent"
+        )
+
+        # Check whether both data.frames are identical
+        expect_equal(
+            colnames(tst), 
+            c("time", "x", "y", "column_1")
+        )
+        expect_equal(
+            tst$column_1, 
+            2 * (1:100)
+        )
+    }
+)
